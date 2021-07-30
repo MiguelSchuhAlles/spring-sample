7545f217b0fb4bb37825d1a5cab7a93fdee10979
Miguel Schuh Alles <miguel.schuh@totvs.com.br>
2021-07-30 17:50:49 -0300

    initial commit

---
+++ src/main/java/com/appsdeveloperblog/app/ws/exceptions/AppExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.appsdeveloperblog.app.ws.exceptions;
+
+import java.util.Date;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.appsdeveloperblog.app.ws.ui.model.response.ErrorMessage;
+
+@ControllerAdvice
+public class AppExceptionHandler extends ResponseEntityExceptionHandler {
+	@ExceptionHandler(value = {Exception.class})
+	public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request) 
+	{
+		var errorDescription = ex.getLocalizedMessage();
+		
+		if(errorDescription == null) {
+			errorDescription = ex.toString();
+		}
+		
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), errorDescription);
+		
+		return new ResponseEntity<>(
+				errorMessage, 
+				new HttpHeaders(), 
+				HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(value = {NullPointerException.class, UserServiceException.class})
+	public ResponseEntity<Object> handleSpecificExceptions(Exception ex, WebRequest request) 
+	{
+		var errorDescription = ex.getLocalizedMessage();
+		
+		if(errorDescription == null) {
+			errorDescription = ex.toString();
+		}
+		
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), errorDescription);
+		
+		return new ResponseEntity<>(
+				errorMessage, 
+				new HttpHeaders(), 
+				HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
